4f26c6ec1e128b7b7bbf863c953aaf87fd53791f
Hartmut Behrens <hartmut.behrens@gmail.com>
2011-09-17 16:24:58 +0200

    added GPRS PM parser

--- lib/Common/ALU/Parse2G.pm
+++ lib/Common/ALU/Parse2G.pm
@@ -7,6 +7,9 @@ Common::ALU::Parse2G;
 #pragmas
 use strict;
 use warnings;
+#modules
+use Compress::Zlib;
+use Time::Local qw(timelocal timelocal_nocheck);
 
 #read PM header records to figure out version, date, time, etc
 sub alu_pm_info {
@@ -73,6 +76,85 @@ sub alu_pm_info {
 	@info{qw/VERSION BSC_ID BSC_NAME STARTTIME STARTDATE ENDTIME ENDDATE SDATE/} = ($rv,$bscId,$bscName,$stime,$sdate,$etime,$edate,$sdate.' '.$stime);
 	return \%info;
 }
+
+sub alu_gpm_info {
+	my ($href) = @_;
+	my @cols = qw/HOUR MIN SEC YEAR MONTH DAY/;
+	my %sdate = ();
+	my %edate = ();
+	my %bssver = (
+		'4' => 'b8',
+		'5' => 'b9',
+		'6' => 'b10',
+		'7' => 'b11');
+		
+	my ($stime,$sdate) = @{$href}{qw/STARTTIME DATE/};
+	@sdate{@cols} = (split(':',$stime),split('-',$sdate));
+	
+	$sdate{'MONTH'}--;
+	@edate{qw/SEC MIN HOUR DAY MONTH YEAR/} = localtime(timelocal_nocheck(@sdate{qw/SEC MIN HOUR DAY MONTH YEAR/}) + 3600);
+	for (@cols) {
+		$edate{$_} = sprintf("%02d",$edate{$_});
+	}
+	$edate{'MONTH'}++;
+	$edate{'YEAR'} += 1900;
+	@{$href}{qw/ENDTIME ENDDATE/} = (join(':',@edate{qw/HOUR MIN SEC/}),join('-',@edate{qw/YEAR MONTH DAY/}));
+	$href->{'STARTDATE'} = $href->{'DATE'};
+	$href->{'SDATE'} = $href->{'DATE'}.' '.$stime;
+	$href->{'VERSION'} = $bssver{$href->{'ITFVERSION'}};
+	delete $href->{'DATE'};
+}
+
+sub parse_gpm {
+	my ($file,$templatedir) = @_;
+	print "Parsing : $file\n";
+	my $rv = 0;
+	my %pm = ();
+	my %counters = ();
+	my %info = ();
+	my @gz = ();
+	my $gz = gzopen($file, "rb") or die "Cannot open $file: $!\n";
+	while ($gz->gzreadline($_) > 0) {
+		chomp;
+		push(@gz,$_);
+	}
+	$gz->gzclose();
+	my $contents = join(';',@gz);
+	
+	for (qw/STARTTIME MFS DATE ITFVERSION/) {
+		($info{$_}) = ($contents =~ /$_\s(.*?);/);
+	}
+	alu_gpm_info(\%info);
+	my $layout_file = $templatedir.'/layout.gpm.'.lc($info{'VERSION'}).'.xml';
+	unless ((-f $layout_file) && (-s $layout_file)) {
+			print "The file $file with version $info{VERSION} cannot be decoded because no layout file could be found (looking for $layout_file) !\n";
+			return $rv;
+		}
+	my $pmLayout = Common::XML::read_xml($layout_file);
+	my @blocks = keys %{$pmLayout->{'blocktype'}};
+	
+	foreach my $item (split(/\}/,$contents)) {
+		next if $item eq ';'; 
+		my ($block,$vals) = ($item =~ /(\w+)\s\{(.*)/);
+		next unless defined $block;
+		next if $block eq '';
+		next unless (grep{/$block/i} @blocks);
+		my ($table,$ix) = @{$pmLayout->{'blocktype'}->{uc($block)}}{qw/loadtable index/};
+		my @ix = split(',',$ix);
+		my %d = ();
+		while ($vals =~ /(\w+)\s(\w+)/g) {
+			$d{$1} = $2;
+			$counters{$table}{$1} = 1;
+		}
+		my $idx = $ix.'='.join(',',@d{@ix});
+		delete @d{@ix};
+		delete @{$counters{$table}}{@ix};
+		
+		@{$pm{$table}{$idx}}{keys %d} = values %d;
+	}
+	return(\%pm,\%counters,\%info);
+}
+
 
 sub decode_t180 {
 	my $infile = shift;
--- lib/Parse/Command/gpm.pm
+++ lib/Parse/Command/gpm.pm
@@ -1,29 +1,30 @@
-package Parse::Command::t180;
+package Parse::Command::gpm;
 
 =head1 NAME
-Parse::Command::alupm;
+Parse::Command::gpm;
 =cut
 #pragmas
 use strict;
 use warnings;
 #modules
-use Common::ALU::Parse2G;
 use Common::CSV;
 use Common::XML;
+use Data::Dumper;
 use File::Path qw(make_path);
 use Parse -command;
 
 sub abstract {
-	return "parse t180 (handover) binary performance management files from Alcatel-Lucent OMC-R into csv files";
+	return "parse gpm (E/GPRS) performance management files from Alcatel-Lucent OMC-R into csv files";
 }
 
 sub usage_desc {
-	return "%c t180 %o [ filename(s) ]";
+	return "%c gpm %o [ filename(s) ]";
 }
 
 sub opt_spec {
 	return (
 	[ "outdir|d=s",	"directory to store parsed csv file(s) in", { default => "../csvload" }],
+	[ "templatedir|t=s",	"directory containing xml templates for decoding pm file", { default => "../templates" }],
 	[ "omc|o=s",	"OMC-R name", { required => 1 }],
 	[ "delete|D",	"Delete file(s) after parsing"],
   );
@@ -43,11 +44,10 @@ sub execute {
 	
 	for my $pmfile (@$args) {
 		
-		my $info = Common::ALU::Parse2G::alu_pm_info($pmfile);
-		my ($pm,$counters) = Common::ALU::Parse2G::decode_t180($pmfile);
+		my ($pm,$counters,$info) = Common::ALU::Parse2G::parse_gpm($pmfile,$opt->{templatedir});
 		warn "Warning: No data was retrieved after parsing $pmfile. This may not be a problem but should be investigated."	unless (scalar(keys %$counters) > 0);
 	
-		my $success = Common::CSV::to_csv($pm,$counters,$info,$opt->{omc},'t180',$opt->{outdir});
+		my $success = Common::CSV::to_csv($pm,$counters,$info,$opt->{omc},'gpm',$opt->{outdir});
 		if ($success && $opt->{delete}) {
 			print "Deleting: $pmfile (-D command line option was provided)\n";
 			unlink($pmfile);
