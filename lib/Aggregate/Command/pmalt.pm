0c19c07dff9e3f95be7af07073c046f42ca7abad
Hartmut Behrens <hartmut.behrens@gmail.com>
2011-10-14 13:13:02 +0200

    updates to alternate aggregation method

--- lib/Aggregate/Command/pmalt.pm
+++ lib/Aggregate/Command/pmalt.pm
@@ -33,10 +33,11 @@ sub opt_spec {
 	[ "host|h=s",	"database host IP address", { required => 1 }],
 	[ "db|d=s",	"database name", { required => 1 }],
 	[ "port|P=s",	"database port", { hidden => 1, default => 3306 }],
-	[ "log|l=s", 	"Log directory", { hidden => 1, default => '../aggregationlog'}],
+	[ "log|l=s", 	"log directory", { hidden => 1, default => '../aggregationlog'}],
 	[ "time|t=s",	"level of aggregation that should be run (specified in aggregation template)", { required => 1, hidden => 1, one_of => \@which}],
 	[ "limit|l=s",	"optionally increase/decrease amount of days to use in aggregation operation"],
 	[ "step|s=s",	"only perform operations from specified step in aggregation template"],
+	[ "debug",	"generate debug output"],
   );
 }
 
@@ -70,10 +71,7 @@ sub instructions {
 	my %todo;
 	
 	for my $counter (keys %$config) {
-		if (config_bad($config->{$counter},['how','do'])) {
-			warn "Skipping aggregation for counter $counter because the instructions in the template are not complete.\n";
-			next;
-		}
+		warn "Skipping aggregation for counter $counter because the instructions in the template are not complete.\n" && next if config_bad($config->{$counter},['how','do']);
 		my $how = $config->{$counter}->{how};
 		foreach my $do ( split(';',$config->{$counter}->{do}) ) {
 			my ($time,$step) = split(':',$do);	#e.g. daily:1
@@ -91,66 +89,70 @@ sub instructions {
 
 sub aggregate {
 	my ($dbh,$template,$opt) = @_;
+	
 	my $today = Common::Date::today();
 	my $config = Common::XML::read_xml($template);
-	my @required = qw/from to identifier groupfrom groupto limit/;
 	my $todo = instructions($config->{aggregate}->{fields}->{field},$opt);
 	
 	for my $step (sort {$a <=> $b} keys %{$config->{aggregate}->{$opt->{time}}->{step}}) {
+		warn "Skipping step $step because command line option \"--step $opt->{step}\" was provided.\n" && next if ( ( defined $opt->{step} ) && ($step != $opt->{step}) );
+		
 		my $sconfig = $config->{aggregate}->{$opt->{time}}->{step}->{$step};
-		if (config_bad($sconfig,\@required)) {
-			warn "Skipped step $step because the instructions in the template are not complete.\n";
-			next;	
-		}
-		my $num = scalar keys %{$todo->{$step}};
+		warn "Skipped step $step because the instructions in the template are not complete.\n" && next if config_bad($sconfig,['from','to','identifier','groupfrom','groupto','limit']);
 		
-		my $dates = get_dates($dbh,$sconfig->{from},$sconfig->{groupfrom},$sconfig->{limit});
-		print "$sconfig->{from} has data for @$dates\n";
-		
-		for my $day (@$dates) {
-			print "Starting aggregation step $step: from $sconfig->{from} to $sconfig->{to} ($num counters) for $day\n";
+		for my $day (get_dates($dbh,$sconfig)) {
 			my $items = get_ids($dbh,$sconfig,$day);
+			my $num = scalar keys %{$todo->{$step}};
+			print "Starting aggregation step $step: from $sconfig->{from} to $sconfig->{to} ($num counters, $#{$items} identifiers) for $day\n";
+			
+			my $select = make_select_sql($sconfig,$todo->{$step});
+			my $sth = $dbh->prepare($select);
 			for my $id (@$items) {
-				my $values = get_grouped_values($dbh,$sconfig,$todo->{$step},$id,$day);	
-				
-			}	
+				print "@$id $day\n" if $opt->{debug};
+				$sth->execute(@$id,$day);
+				my @row = $sth->fetchrow_array;
+				update_db($dbh,$sconfig,[split(',',$sconfig->{identifier}),keys %{$todo->{$step}}],[@$id,@row]);
+			} 	
 		}
-		
-		
 	}
 }
 
-sub get_grouped_values {
-	my ($dbh,$sconfig,$todo,$id_vals,$groupval) = @_;
-	my %values;
-	my $sth;
-	
-	foreach my $counter (keys %$todo) {
-		
-		my $what = ($todo->{$counter} =~ /[\+|\-|\*|\/]/) ? $todo->{$counter} : $todo->{$counter}.'(`'.$counter.'`)' ; #translate xml instruction to something that can run in sql
-		my $where = join(' and ',map("$_ = ?", split(',',$sconfig->{identifier}),$sconfig->{groupfrom} ) );
-		my $sql = "select $what from $sconfig->{from} where $where";
-		#print $sql,"\n";
-		$sth = $dbh->prepare($sql);
-		$sth->execute(@$id_vals,$groupval);
-		my @row = $sth->fetchrow_array;
-		my $val = $row[0] || '';
-		print $sql,"\n";
-		print "@$id_vals $groupval :  $val \n";
-		
+sub get_additional {
+	my ($opt,$sconfig,$id,$day) = @_;
+	my (@cols,@vals);
+	return (\@cols,\@vals);
+}
+
+sub update_db {
+	my ($dbh,$sconfig,$cols,$vals) = @_;
+	#print $cols,"\n";
+	my @values = map( defined $_ ? $_ : 'NULL', @$vals);
+	my $replace = 'replace into '.$sconfig->{to}.' ('.join(',',map('`'.$_.'`',@$cols)).') values ('.join(',',map($_ eq 'NULL' ? 'NULL' :  '\''.$_.'\'',@values)).')';
+	$dbh->do($replace) || die $replace;
+}
+
+
+sub make_select_sql {
+	my ($sconfig,$todo) = @_;
+	my @what;
+	my $where = join(' and ',map("$_ = ?", split(',',$sconfig->{identifier}),$sconfig->{groupfrom} ) );
+	for my $counter (keys %$todo) {
+		my $what = ($todo->{$counter} =~ /[\+|\-|\*|\/]/) ? $todo->{$counter} : $todo->{$counter}.'(`'.$counter.'`)';
+		push @what, $what;
 	}
-	return \%values;
+	my $sql = "select ".join(',',@what)." from $sconfig->{from} where $where";
+	return $sql;
 }
 
 sub get_dates {
-	my ($dbh,$table,$group_col,$limit) = @_;
+	my ($dbh,$sconfig) = @_;
 	my @group;
-	my $sth = $dbh->prepare("select distinct $group_col from $table order by $group_col desc limit $limit");
+	my $sth = $dbh->prepare("select distinct $sconfig->{groupfrom} from $sconfig->{from} order by $sconfig->{groupfrom} desc limit $sconfig->{limit}");
 	$sth->execute();
 	while (my @row = $sth->fetchrow_array) {
 		push(@group,$row[0]);
 	}
-	return \@group;
+	return @group;
 }
 
 sub get_ids {
@@ -173,143 +175,6 @@ sub config_bad {
 		$rv = 1 unless exists $config->{$_};
 	}
 	return $rv;
-}
-
-sub get_items {
-	my ($dbh,$table,$col) = @_;
-	my $sth = $dbh->prepare('select distinct '.$col.' from '.$table);
-	$sth->execute;
-	
-} 
-
-
-sub do_aggregation {
-	my ($dbh,$template,$opt) = @_;
-	my $atime = $opt->{time};
-	my $uplimit = $opt->{limit};
-	my $config = Common::XML::read_xml($template);
-	my %todo = ();
-	my %need = ();
-	my %have = ();
-	my %seen = ();
-	my %cmap = ();
-	
-	foreach my $col (keys %{$config->{'aggregate'}->{'fields'}->{'field'}}) {
-		my @op = split(';',$config->{'aggregate'}->{'fields'}->{'field'}->{$col}->{'do'});
-		my $how = defined($config->{'aggregate'}->{'fields'}->{'field'}->{$col}->{$atime}) ? $config->{'aggregate'}->{'fields'}->{'field'}->{$col}->{$atime} : $config->{'aggregate'}->{'fields'}->{'field'}->{$col}->{'how'};
-		foreach my $op (@op) {
-			my ($type,$which) = split(':',$op);
-			next unless ($type eq $atime);
-			if ($which =~ /(\d+)-(\d+)/) {
-				for ($1..$2) {
-					$todo{$how}{$_}{$col} = 1;
-				}
-			}
-			else {
-				my @t = split(',',$which);
-				for (@t) {
-					$todo{$how}{$_}{$col} = 1;
-				}
-			}
-		}
-	}
-	#print Dumper(\%todo);
-	#exit;
-	if (%todo) {
-		foreach my $how (keys %todo) {
-		#foreach my $how (qw/sum/) {
-			foreach my $step (sort {$a<=>$b} keys %{$todo{$how}}) {
-			if ( (exists $opt->{step}) && ($step != $opt->{step}) ) {
-				warn "Skipping operations for step $step because command line option --step with value \"$opt->{step}\" was provided.\n";
-				next;
-			}
-			#foreach my $step (7..8) {
-				
-				next unless exists $config->{'aggregate'}->{$atime}->{'step'}->{$step};
-				my ($from,$to,$groupFrom,$groupTo,$limitTo,$limitFrom,$limit) = @{$config->{'aggregate'}->{$atime}->{'step'}->{$step}}{qw/from to groupfrom groupto limitto limitfrom limit/};
-				if ($uplimit) {
-					warn "User selected limit of \"$uplimit\" will override limit of \"$limit\" defined in template.\n";
-					$limit = $uplimit;
-				}
-				$groupTo = $groupFrom if (lc($groupTo) eq 'groupfrom');
-				my @dates = ();
-				print "Checking latest dates in $from ..";
-				my $dates = 'select distinct '.$limitFrom.' from '.$from.' order by '.$limitFrom.' desc limit '.$limit;
-				my $sth = $dbh->prepare($dates);
-				$sth->execute();
-				while (my @row = $sth->fetchrow_array) {
-					#print "$row[0]..";
-					push(@dates,$row[0]);
-				}
-				#if (defined $args{s}) {
-				#	my $ix = int $args{s};
-				#	@dates = $dates[$ix];
-				#}
-				print "found: @dates\n";
-				foreach my $day (@dates) {
-					#see which data needs to be aggregated
-					print "Aggregating $from to $to for $day.\n";
-					my @cols = sort keys %{$todo{$how}{$step}};
-					if ($how =~ /[\+|\-|\*|\/]/) {
-						@{$cmap{$from}}{@cols} = map($how,@cols);
-					}
-					else {
-						@{$cmap{$from}}{@cols} = map($how.'(`'.$_.'`)',@cols);
-					}
-					@{$cmap{$to}}{@cols} = map('`'.$_.'`',@cols);
-					print Dumper(\%cmap);
-					exit;
-					my @grpT = split(',',$groupTo);
-					my @grpF = split(',',$groupFrom);
-					unless ($#grpT == $#grpF) {
-						die "Aggregation from $from to $to not possible because number of grouping elements specified in $template are not equal(groupfrom has ".($#grpT+1)." elements, groupto has ".($#grpF+1).").\n";
-						next;
-					}
-					
-					my %d = ();
-					my $need = 'select '.join(',',@{$cmap{$from}}{@cols}).','.$groupFrom.' from '.$from.' where '.$limitFrom.'=? group by '.$groupFrom;
-					#my $have = 'select '.join(',',@{$cmap{$to}}{@cols}).','.$groupTo.' from '.$to.' where '.$limitTo.'=? group by '.$groupTo;
-					unless (exists $seen{$to}{$day}) {
-						my $have = 'select '.$groupTo.' from '.$to.' where '.$limitTo.'=? group by '.$groupTo;
-						$sth = $dbh->prepare($have);
-						$sth->execute($day);
-						
-						while (my @row = $sth->fetchrow_array) {
-							@d{@grpT} = @row;
-							$have{$to}{join(',',map(defined($_)?$_:'',@d{@grpT}))} = 1;
-						}
-						%d = ();
-					}
-					
-					#check what is available and aggregate items as required
-					print $need,"\n";
-					#$sth = $dbh->prepare($need);
-					#$sth->execute($day);
-					#while (my @row = $sth->fetchrow_array) {
-					#	@d{@cols,@grpT} = map(defined($_)?$_:0,@row);
-					#	my $idx = join(',',@d{@grpT});
-					#	if (exists($have{$to}{$idx})) {
-					#		my $update = 'update '.$to.' set '.join(',',map('`'.$_.'`='.($d{$_} || 0),@cols)).' where '.join(' and ',map($_.'=\''.$d{$_}.'\'',@grpT));
-							#print "update: $update\n";
-							#$dbh->do($update) || die $update;
-					#		$seen{$to}{$day} = 1;
-							#print "$update\n";
-					#	}
-					#	else {
-					#		my $replace = 'replace into '.$to.' ('.join(',',map('`'.$_.'`',@grpT,@cols)).') values ('.join(',',map('\''.$_.'\'',@d{@grpT,@cols})).')';
-							#print "replace: $replace\n";
-							#$dbh->do($replace) || die $replace;
-							#print "$replace\n";
-					#	}
-					#}
-					%cmap = ();
-				}
-			}
-		}
-	}
-	else {
-		print "There is nothing to do for \"$atime\" aggregation - perhaps no aggregation operation was specified in $template?\n";
-	}
 }
 
 1;
