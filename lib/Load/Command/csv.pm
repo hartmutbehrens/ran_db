f6bfb3bc1f3c310cf365d284b3e68fefe434416e
Hartmut Behrens <hartmut.behrens@gmail.com>
2011-11-08 13:25:18 +0200

    rewrite rdb-load csv command to not be dependant on csvload.xml config anymore

--- lib/Load/Command/csv.pm
+++ lib/Load/Command/csv.pm
@@ -9,7 +9,8 @@ use warnings;
 
 #modules
 use Common::MySQL;
-use Common::CSV;
+#use Common::CSV;
+use Data::Dumper;
 use Load -command;
 
 sub abstract {
@@ -23,6 +24,9 @@ sub usage_desc {
 sub opt_spec {
 	return (
 	[ "csvdir|c=s",	"directory to load csv files from", { default => "../csvload" }],
+	[ "fsep|f=s",	'input field separator (default ;)', { default => ';' }],
+	[ "hline=s",	'specify header line. [line#, col1, col2, ..] to specify header column names', ],
+	[ "cline=s",	'specify column line. [line#, col1, col2, ..] to manually specify column names or [line#, auto] to extract column names automatically', { required => 1 }],
 	[ "user|u=s",	"database user", { required => 1 }],
 	[ "pass|x=s",	"database password", { required => 1 }],
 	[ "host|h=s",	"database host IP address", { required => 1 }],
@@ -55,7 +59,8 @@ sub execute {
 		my $match = "$opt->{nms}.$opt->{type}.";
 		next unless ($file =~ /$match/);
 		print "About to load: $file\n";
-		 my $success = Common::CSV::load_csv($dbh,$opt->{csvdir},$file,$opt->{config});
+		 #my $success = Common::CSV::load_csv($dbh,$opt->{csvdir},$file,$opt->{config});
+		 my $success = load_csv($dbh,$opt,$file);
 		 $count++ if $success;
 		 if ($success && $opt->{delete}) {
 			print "Deleting: $file (-D command line option was provided)\n";
@@ -65,6 +70,77 @@ sub execute {
 	if ($count == 0) {
 		warn "No files of type \"$opt->{type}\" from \"$opt->{nms}\" could be loaded from \"$opt->{csvdir}\" !\n";
 	}
+}
+
+sub load_csv {
+	my ($dbh,$opt,$file) = @_;
+	my ($source,$type,$table) = split('\.',$file);
+	
+	unless (defined($source) && defined($type) && defined($table)) {
+		warn "$file does not conform to the naming convetion \"source.type.table\" and will be skipped ..\n";
+		return 0;
+	}
+	
+	#check if table exists
+	return 0 unless Common::MySQL::has_table($dbh,$table);
+	#get table definition
+	my $def = Common::MySQL::get_definition($dbh,$table);
+	$dbh->do("lock tables $table write") || die($dbh->errstr) ;
+	print "Loading: ($file) $table from $source ($type)..\n";
+	
+	my ($i,$has_cols) = (0,0);
+	
+	my (%d,%only,@cols);
+	open my $in ,"<",$opt->{csvdir}.'/'.$file || die "Could not open $opt->{csvdir}/$file for reading:$!\n";
+	while(<$in>) {
+		chomp;
+		
+		if (defined $opt ->{hline} && ($opt->{hline} =~ /^$./) ) {
+			my (undef,@hcols) = split('\W+',$opt->{hline});
+			@d{@hcols} = split($opt->{fsep}, $_);
+			for (@hcols) {
+				$only{$_} = 1 if exists $def->{$_};
+			}
+			next;
+		}
+		
+		if ($opt->{cline} =~ /^$./) {
+			my (undef,@ccols) = split('\W+',$opt->{cline});
+			@cols = lc($ccols[0]) eq 'auto' ? split($opt->{fsep}, $_) : @ccols;
+			for (@cols) {
+				$only{$_} = 1 if exists $def->{$_};
+			}
+			$has_cols = 1;
+			next;
+		}
+		
+		next unless $has_cols;
+		@d{@cols} = split($opt->{fsep}, $_);
+		my %e = ();
+		@e{keys %only} = @d{keys %only};
+		#remove clutter
+		foreach my $k (keys %e) {
+			delete $e{$k} if (defined($e{$k}) && (uc($e{$k}) eq 'NULL'));
+			delete $e{$k} if (defined($e{$k}) && (uc($e{$k}) eq '?'));	#obsynt
+			delete $e{$k} if (defined($e{$k}) && (length($e{$k}) == 0) );
+		}
+		#fixed in to_csv for most file types, but not for rnl (because it is extracted as-is out of the archive coming from the OMC-R)
+		if (exists $def->{'OMC_ID'}) {
+			$e{'OMC_ID'} = exists $e{'OMC_ID'} ? $e{'OMC_ID'} : $source;	
+		}
+		my @vals = values %e;
+		#my $sql = 'replace into '.$table.' ('.join(',',map('`'.$_.'`',keys %e)).') values ('.join(',',map('\''.$_.'\'',@vals)).')';
+		my $sql = 'replace into '.$table.' ('.join(',',map('`'.$_.'`',keys %e)).') values ('.join(',',map('?',@vals)).')';
+		my $sth = $dbh->prepare($sql);
+		$sth->execute(@vals);
+		#print "$sql\n";
+		$i++;
+		
+	}
+	close $in;
+	$dbh->do("unlock tables") || die($dbh->errstr) ;
+	print "Loaded $i records.\n";
+	return 1;	
 }
 
 1;
