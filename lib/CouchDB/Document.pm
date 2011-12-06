973513af105791abbf828a6e4ea789f1999d267b
Hartmut Behrens <hartmut.behrens@gmail.com>
2011-12-06 16:00:45 +0200

    start fleshing out get and put methods for CouchDB documents

--- lib/CouchDB/Document.pm
+++ lib/CouchDB/Document.pm
@@ -18,9 +18,15 @@ extends 'CouchDB::Database';
 
 has json => ( is => 'rw', default => sub { return Mojo::JSON->new } );
 
-#before 'put' => \&_check_put;
+before 'get' => \&_check_for_id;
+before 'put' => \&_check_for_id;
 
 sub get {
+	my ($self,$id) = @_;
+	my $headers = { 'Cache-Control' => 'no-cache', 'Content-Type' => 'application/json' };
+	my $response = CouchDB::Request->new(uri => $self->uri.$id, headers => $headers)->get;
+	return $response->json if defined $response->json;
+	confess "A response was not received when getting doc $id.\n";
 	
 }
 
@@ -30,8 +36,12 @@ sub head {
 
 #store a doc in couchdb with _id defined
 sub put {
-	my ($self,$doc) = @_;
+	my ($self,$id,$doc) = @_;
 	my $headers = { 'Cache-Control' => 'no-cache', 'Content-Type' => 'application/json' };
+	my $response = CouchDB::Request->new(uri => $self->uri.$id, headers => $headers)->put;
+	return 1 if (defined $response->code) && ($response->code == 201);
+	confess "The document $id already exists.\nCheck that the correct _rev was specified.\n" if (defined $response->code) && ($response->code == 409);
+	confess "The document $id could not be PUT.\n";
 }
 
 #store a doc in couchdb and let couchdb come up with a unique id
@@ -43,8 +53,8 @@ sub delete {
 	
 }
 
-sub _check_put {
-	confess "A _id field is required to PUT documents.\n" unless defined $_[1]->{_id};
+sub _check_for_id {
+	confess "A _id field is required.\n" unless defined $_[1];
 }
 
 
