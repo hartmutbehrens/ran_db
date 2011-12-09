21e3dfc6aa698721ca875923115dff04b39a2069
Hartmut Behrens <hartmut.behrens@gmail.com>
2011-12-09 13:42:31 +0200

    place _id and _rev checks in a before => sub call

--- lib/CouchDB/Document.pm
+++ lib/CouchDB/Document.pm
@@ -14,36 +14,41 @@ use CouchDB::Request;
 use Data::Dumper;
 use Moo;
 
-has _id => ( is => 'rw');
+has _id => ( is => 'rw' );
+has _rev => ( is => 'rw' );
 has debug => (is => 'rw', default => sub { return 0} );
 has db => ( is => 'rw', isa => sub { confess "CouchDB::Database required" unless ref $_[0] eq 'CouchDB::Database' }, required => 1 );
 
 before 'put' => \&_check_put;
+before 'get' => \&_get_id_rev;
+before 'doc_uri' => \&_get_id_rev;
 
 sub get {
-	my ($self,$id) = @_;
-	$self->_id($id) if defined $id;
-	confess "_id required for GET.\n" unless defined $self->_id; 
+	my $self = shift;
+	 
 	my $request = CouchDB::Request->new(uri => $self->doc_uri, debug => $self->debug, method => 'get');
 	my $response = $request->execute;
-	return $response->json if defined $response->json && $response->code == 200;
+	if (defined $response->json && $response->code == 200) {
+		$self->_rev($response->json->{_rev});
+		return $response->json;
+	}
 	$request->complain($response);
 }
 
-sub head {
-	
-}
 
 sub rev {
-	my $self = shift;
-	
+	my ($self,$id) = @_;
+	return $self->_rev if defined $self->_rev;
+	$self->_id($id) if defined $id;
+	$self->get; 
+	return $self->_rev;
 }
 
 sub doc_uri {
-	my ($self,$id) = @_;
-	$self->_id($id) if defined $id;
-	confess "_id required for doc_uri.\n" unless defined $self->_id;
-	return $self->db->db_uri.$self->_id; 
+	my $self = shift;
+	my $uri = $self->db->db_uri.$self->_id;
+	$uri .= '?rev='.$self->_rev if defined $self->_rev;
+	return $uri; 
 }
 
 
@@ -70,6 +75,12 @@ sub delete {
 
 sub _check_put {
 	confess "Doc parameter has not been provided for PUT.\n" unless defined $_[1];
+}
+
+sub _get_id_rev {
+	$_[0]->_id($_[1]) if defined $_[1];
+	$_[0]->_rev($_[2]) if defined $_[2];
+	confess "_id required.\n" unless defined $_[0]->_id;
 }
 
 1;
