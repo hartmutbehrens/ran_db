76fd7fc326416bbaf3d0ba98a5f18f74a03c4987
Hartmut Behrens <hartmut.behrens@gmail.com>
2011-12-09 14:13:01 +0200

    more consisten id checks

--- lib/CouchDB/Document.pm
+++ lib/CouchDB/Document.pm
@@ -19,9 +19,10 @@ has _rev => ( is => 'rw' );
 has debug => (is => 'rw', default => sub { return 0} );
 has db => ( is => 'rw', isa => sub { confess "CouchDB::Database required" unless ref $_[0] eq 'CouchDB::Database' }, required => 1 );
 
-before 'put' => \&_check_put;
-before 'get' => \&_get_id_rev;
-before 'doc_uri' => \&_get_id_rev;
+before 'put' => \&_check_id;
+before 'get' => \&_check_id;
+before 'rev' => \&_check_id;
+before 'doc_uri' => \&_check_id;
 
 sub get {
 	my $self = shift;
@@ -37,9 +38,8 @@ sub get {
 
 
 sub rev {
-	my ($self,$id) = @_;
+	my $self = shift;
 	return $self->_rev if defined $self->_rev;
-	$self->_id($id) if defined $id;
 	$self->get; 
 	return $self->_rev;
 }
@@ -55,9 +55,8 @@ sub doc_uri {
 
 #store a doc in couchdb with _id defined
 sub put {
-	my ($self,$doc,$id) = @_;
-	$self->_id($id) if defined $id;
-	confess "_id required for PUT.\n" unless defined $self->_id; 
+	my ($self,$id,$doc) = @_;
+	confess "Doc parameter has not been provided for PUT.\n" unless defined $doc; 
 	my $request = CouchDB::Request->new(uri => $self->doc_uri, debug => $self->debug, method => 'put');
 	my $response = $request->execute;
 	return 1 if (defined $response->code) && ($response->code == 201);
@@ -74,12 +73,11 @@ sub delete {
 }
 
 sub _check_put {
-	confess "Doc parameter has not been provided for PUT.\n" unless defined $_[1];
+	
 }
 
-sub _get_id_rev {
+sub _check_id {
 	$_[0]->_id($_[1]) if defined $_[1];
-	$_[0]->_rev($_[2]) if defined $_[2];
 	confess "_id required.\n" unless defined $_[0]->_id;
 }
 
