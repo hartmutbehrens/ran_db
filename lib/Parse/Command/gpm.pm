79bbc0a042a314cdc06b699e53632949787d970d
Hartmut Behrens <hartmut.behrens@gmail.com>
2012-02-21 15:36:28 +0200

    parallelize gpm processing

--- lib/Parse/Command/gpm.pm
+++ lib/Parse/Command/gpm.pm
@@ -12,6 +12,7 @@ use Common::CSV;
 use Common::Lock;
 use Common::XML;
 use File::Path qw(make_path);
+use Parallel::ForkManager; 
 use Parse -command;
 
 sub abstract {
@@ -46,7 +47,9 @@ sub execute {
 	my $lock = '.'.$opt->{omc}.'gpm';
 	Common::Lock::get_lock($lock) or Common::Lock::bail($lock);
 	
+	my $pfm = Parallel::ForkManager->new($opt->{parallel});
 	for my $pmfile (@$args) {
+		$pfm->start and next; # do the fork
 		
 		my ($pm,$counters,$info) = Common::ALU::Parse::2G::parse_gpm($pmfile,$opt->{templatedir});
 		warn "Warning: No data was retrieved after parsing $pmfile. This may not be a problem but should be investigated."	unless (scalar(keys %$counters) > 0);
@@ -56,7 +59,10 @@ sub execute {
 			print "Deleting: $pmfile (-D command line option was provided)\n";
 			unlink($pmfile);
 		}
+		$pfm->finish; # do the exit in the child process
+		
 	}
+	$pfm->wait_all_children;
 }
 
 1;
